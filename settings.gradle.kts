356d91ed4ca3a8a88b72907a5fcdb79bd950714a
Filip Luchianenco <filipluch@gmail.com>
2026-02-12 12:21:54 -0600

    chore: initialize project with build system and Gradle configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "acp"
+
+include(":sdk:annotations")
+include(":sdk:runtime")
+include(":sdk:compiler")
+include(":hub:app")
+include(":sample:musicapp")
